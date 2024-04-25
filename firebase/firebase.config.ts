5e14ddd51dfe35af98f49c0238d1340c987cee23
KamaProgman <latipovkamran1@gmail.com>
2024-04-25 14:10:03 +0500

    fixing

--- firebase/firebase.config.ts
+++ firebase/firebase.config.ts
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-	databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAgAICJRXPle0dNfCfgMkG-hcV949YkVsg",
+  authDomain: "flexibble-4943a.firebaseapp.com",
+  databaseURL: "https://flexibble-4943a-default-rtdb.firebaseio.com",
+  projectId: "flexibble-4943a",
+  storageBucket: "flexibble-4943a.appspot.com",
+  messagingSenderId: "375706988990",
+  appId: "1:375706988990:web:c76bb72d056397c52c09ef",
+  measurementId: "G-H4WQBN15CZ"
 };
 
 export const app = initializeApp(firebaseConfig);
