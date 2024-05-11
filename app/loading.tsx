478842d14e8be01fda60d318639224b3da8a369a
KamaProgman <latipovkamran1@gmail.com>
2024-05-11 13:29:53 +0500

    test loader

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="paddings min-h-[100vh] text-3xl font-bold text-primary-purple flex items-center justify-center">
+      Loading...
+    </div>
+  );
+};
+
+export default Loading
--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,11 +1,26 @@
 "use client"
 import { signIn } from "next-auth/react";
 import Button from "./Button";
+import { useState } from "react";
 
 export const AuthProvider = () => {
+  const [isSigning, setIsSigning] = useState(false);
+
+  const handleSignIn = async () => {
+    setIsSigning(true)
+    try {
+      await signIn('google')
+    } catch (error) {
+      throw error
+    }
+    finally {
+      setIsSigning(false)
+    }
+  }
+
   return (
     <>
-      <Button title="Sign In" handleClick={() => signIn('google')} />
+      <Button title="Sign In" handleClick={handleSignIn} isSubmitting={isSigning} />
     </>
   )
 }
